fbf53c1dc66d4ed27d37789477723dd092ec6ae2
abharatham <abharatham123@gmail.com>
2025-04-27 17:07:42 -0400

    Challenge complete on dashboard and challenge page

--- fitjacket/challenges/models.py
+++ fitjacket/challenges/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.conf import settings
 
 class Challenge(models.Model):
     title       = models.CharField(max_length=200)
     description = models.TextField()
     start_date  = models.DateField()
     end_date    = models.DateField()
+
+class Participation(models.Model):
+    user         = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    challenge    = models.ForeignKey(Challenge, on_delete=models.CASCADE)
+    joined_at    = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.title
--- fitjacket/challenges/urls.py
+++ fitjacket/challenges/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('',          views.challenge_list, name='list'),
     path('join/<int:pk>/', views.join_challenge, name='join'),
     path('create/', views.create_challenge, name='create'),
+    path(
+        'participation/<int:participation_id>/complete/',
+        views.complete_challenge,
+        name='complete'
+    ),
 ]
